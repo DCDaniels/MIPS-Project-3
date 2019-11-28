c2679886370e15c84dcea6ba6dd6abd318dba5a5
David <davidcharlesdaniels@gmail.com>
2019-11-28 01:41:32 -0500

    Declarations

--- project3.s
+++ project3.s
@@ -2,3 +2,8 @@
 #02884990%11 = 9
 #26 + 9 = Base 35
 #Range (0,y) and (0-Y)
+
+.data 						#Declarations
+InputVariable: .space 1000			#Variable for user input 
+list: .word 0, 0, 0, 0				#Created a word list
+BadInput: .asciiz "Invalid input"		#Variable used to output Invalid input
