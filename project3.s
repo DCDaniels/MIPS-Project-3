ea1d9b28be7ced29ca8345981e6169fd5036ba9c
David <davidcharlesdaniels@gmail.com>
2019-11-28 01:43:25 -0500

    Beginning of Main

--- project3.s
+++ project3.s
@@ -11,4 +11,10 @@ BadInput: .asciiz "Invalid input"		#Variable used to output Invalid input
 .text						#Instructions stored in text segment at next available address
 .globl main					#Allows main to be refrenced anywhere
 
-
+main:
+	li $v0, 8				#Allows user to input
+	la $a0, InputVariable			#Saves input to  variable
+	li $a1, 1001				#Allows the input to be 1000 characters
+	syscall 				#Issues a System Call
+	
+	
