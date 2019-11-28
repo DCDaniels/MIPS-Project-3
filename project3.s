6e4106a35f37c44eb30cc58c174a110bd6599e5a
David <davidcharlesdaniels@gmail.com>
2019-11-28 18:08:28 -0500

    Deleted unneccessary functions

--- project3.s
+++ project3.s
@@ -34,35 +34,4 @@ main:
 		li $t4, 0x0A			#Loads a newline
 		li $t6, 0			#Length of substring
 	
-	loop:
-		bgt $t5,$t2, output_bad_input		#Branch if more than 4 good characters
-		lb $t6,0($t1)				#Gets each integer from the input
-		j check_character			#Jump to check character function if it doesnt branch
 	
-	output_bad_input:			#Fucntion to print invalid output
-	li $v0,4
-	la $a0, BadInput			#Prints Invalid input
-	syscall					#Issues a System Call
-	
-	
-	beginning_characters:			#Function that checks if character is a space or tab
-	beq $t6,$t3, skip_character		#Branches if character is a space
-	beq $t6,$t4, skip_character		#Branches if character is a tab
-	j check_character			#Jump to check_character
-	
-	skip_character:				#Function that moves to next character
-	addi $t1,$t1, 1				#Increments $t1 to check next number
-	j loop					#Jumps back to beginning of loop
-	
-	check_character:			#Function to see if character is invalid
-	blt $t6,$s0, output_bad_input		#Branch if character is below ASCII number for 0
-	bgt $t6,$s1, check_capital		#Branch if character is above ASCII number for 9
-	addi $t6,$t6, -48			#Convert it to its decimal counterpart
-	j incrementor				#Jump to incrementor function
-	
-	incrementor:				#Function to increment all variables
-	sb $t6, 0($s6)		 		#Stores the character in a string
-	addi $s6,$s6, 1 			#Increment array posistion
-	addi $t1,$t1, 1 			#Increment the input string
-	addi $t5,$t5, 1 			#Increment the number of valid characters
-	j loop					#Go back to loop
